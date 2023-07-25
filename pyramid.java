59690325acdc5f33f3cb6cf5ba39883f0c2cf0f6
Pradyut30 <121106850+Pradyut30@users.noreply.github.com>
2023-07-25 16:47:32 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,25 @@
+package pradyut;
+import java.util.*;
+public class pyramid {
+public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the levels of pyramid:");
+	int levels=sc.nextInt();
+	for(int row=1;row<levels+1;row++)
+	{
+		for(int space=row;space<levels;space++)
+		{
+			System.out.print(" ");
+		}
+		for(int col=row;col<=2*row-1;col++)
+		{
+			System.out.print(col+" ");
+		}
+		for(int k=2+row-2;k>=row;k--)
+		{
+			System.out.print(k+" ");
+		}
+		System.out.println();
+	}
+}
+}
